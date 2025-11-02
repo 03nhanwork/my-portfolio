59d1b9e7a16d97368cd21b0e66732009d1d4ff3f
03nhanwork <03nhanwork@gmail.com>
2025-11-01 22:02:02 -0700

    Update vite.config.js
    base location

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '03nhanwork.github.io', // Add this line
+  base: 'my-portfolio', // Add this line
 })
